88c8de04d7d900867de4effae3dab3b2feed3574
extrahypo <matcour@windowslive.com>
2012-10-15 12:58:12 +1300

    Update src_dev/SOURCE_A2/TeachesManager.java
    Add teaches instance

--- src_dev/SOURCE_A2/TeachesManager.java
+++ src_dev/SOURCE_A2/TeachesManager.java
@@ -1,8 +1,10 @@
 public class TeachesManager {
 	static private TeachesManager singleton = null;
+	private LinkedList<Teaches> teaches;
 	
 	private TeachesManager() {
-
+		assert(singleton == null);
+		this.teaches = new LinkedList<Teaches>();
 	}
 
 	public static TeachesManager getInstance() {
@@ -10,4 +12,18 @@ public class TeachesManager {
 			singleton = new TeachesManager();
 		return singleton;
 	}
+
+	public boolean add(Class c, Teacher t){
+		if(c != null && t != null){
+			Teaches teach = new Teaches(c, t);
+			if(!teaches.contains(teach){
+				teaches.add(teach);
+				return true;
+			}
+		}
+		return false;
+
+	}
+	
+
 }
