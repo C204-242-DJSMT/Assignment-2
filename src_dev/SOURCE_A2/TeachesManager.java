b0bb24029336ed8cd140311021cd515cae63dd61
extrahypo <matcour@windowslive.com>
2012-10-15 16:20:24 +1300

    Update for delete teaches

--- src_dev/SOURCE_A2/TeachesManager.java
+++ src_dev/SOURCE_A2/TeachesManager.java
@@ -1,10 +1,12 @@
 public class TeachesManager {
 	static private TeachesManager singleton = null;
 	private LinkedList<Teaches> teaches;
+	private TimeOutList<Teaches> deleteedTeaces;
 	
 	private TeachesManager() {
 		assert(singleton == null);
 		this.teaches = new LinkedList<Teaches>();
+		this.deletedTeaches = new TimeOutList<Teaches>();
 	}
 
 	public static TeachesManager getInstance() {
@@ -22,7 +24,58 @@ public class TeachesManager {
 			}
 		}
 		return false;
+	}
 
+	/**
+	 *
+	 */
+	public void delete(Teacher t, Class c) {
+		if(t != null && c != null){
+			Teaches delete = new Teaches(c,t);
+			if(teaches.contains(delete)){
+				teaches.remove(delete);
+				deletedTeaches.add(delete);
+
+				if(!checkEntry(delete, true))
+					delete(t,c);
+			}
+		}
+	}
+
+	/*
+	* Checks if its in temporary list and not in teaches List if delete is true
+	* Checks if its in teaches list anbd not in deleted if delete is false
+	*/
+	private booleen checkEntry(Teaches e, boolean delete){
+		assert(e != null);
+		if(delete == true){
+			if(!teaches.contains(e) && deletedTeaches.contains(e))
+				return true;
+
+			return false;
+		}
+		else{
+		if(teaches.contains(e) && !deletedTeaches.contains(e))
+				return true;
+
+			return false;	
+		}
+	}
+	
+	/*
+	* Restores enrty from deleted list to teaches list
+	*/
+	public void restore(Teacher t, Class c){
+		if(C != null && t != null){
+			Teaches restore = new Teaches(c,t);
+			if(deletedTeaches.contains(restore)){
+				deletedTeaches.remove(restore);
+				teaches.add(restore);
+
+				if(!checkEntry(restore, false))
+					restore(t,c);
+			}
+		}
 	}
 	
 
